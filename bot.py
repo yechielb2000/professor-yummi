6567fab9805d6492211275a95f54176e363bcac4
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-07-19 02:12:53 +0300

    add commands

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import message
 from discord.ext import commands
 from sources import DISCORD_TOKEN
 import riot
@@ -9,8 +10,14 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready.')
 
-@client.command()  
-async def stats(contex, *,summonerName):
-    await contex.send(riot.printStats(summonerName))
+# this command clear messages (the user can choose how many messages to delete the default is 1)
+@client.command()
+async def clear(ctx, amount = 1):
+    await ctx.channel.purge(limit=amount)
+
+# this command let the user know what is the rank stats of a player in euw1 server by typing .stats 'name of summoner'
+@client.command(aliases=['rank'])  
+async def stats(ctx, *,summonerName):
+    await ctx.send(riot.printStats(summonerName))
 
 client.run(DISCORD_TOKEN.token)   
--- riot.py
+++ riot.py
@@ -3,13 +3,15 @@ from sources import API_KEY
 import math
 watcher = LolWatcher(API_KEY.key)
 
+
 def printStats(summonerName):
     
     summoner = watcher.summoner.by_name('euw1', summonerName)
     stats = watcher.league.by_summoner('euw1', summoner['id'])
+    # mastaries = watcher.champion_mastery.by_summoner('euw1', summoner['id']) for all champions mastery
 
     try:
-        return summonerName + "\nSolo/Due:" + str(flex_solo(stats, 0)) + "\nFlex 5V5:" + str(flex_solo(stats, 1))
+        return 'Level : ' + str(summoner['summonerLevel']) + "\n" + 'Flex 5v5 : ' + flex_solo(stats, 0)[0] + " " + flex_solo(stats, 0)[1] + " " + flex_solo(stats, 0)[2] + " " + flex_solo(stats, 0)[3] + "\n" + 'Solo Due : ' +  flex_solo(stats, 1)[0] + " " + flex_solo(stats, 1)[1] + " " + flex_solo(stats, 1)[2] + " " + flex_solo(stats, 1)[3]
     except:
         return summonerName + "\nNo stats"
 
@@ -21,4 +23,10 @@ def flex_solo(stats, pos):
     wins = stats[pos]['wins']
     losses = stats[pos]['losses']
     winrate = wins / (wins + losses)
-    return tier, rank, str(lp) + "LP", "winrate :", str(int(math.ceil(winrate * 100))) + "%"
+
+    result =  [tier, rank, str(lp) + "LP", "with " + str(int(math.ceil(winrate * 100))) + "% winrate"]
+    return result
+
+printStats("Name")
+
+   
